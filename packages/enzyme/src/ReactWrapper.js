a59cb0828237c479cb9e00ad79084da00c050dd8
Jordan Harband <ljharb@gmail.com>
2018-09-03 00:43:21 -0700

    [New] `mount`: `.state()`/`.setState()`: allow calling on children.
    Fixes #635. Fixes #1289.

--- packages/enzyme/src/ReactWrapper.js
+++ packages/enzyme/src/ReactWrapper.js
@@ -229,13 +229,13 @@ class ReactWrapper {
    * Forces a re-render. Useful to run before checking the render output if something external
    * may be updating the state of the component somewhere.
    *
-   * NOTE: can only be called on a wrapper instance that is also the root instance.
+   * NOTE: no matter what instance this is called on, it will always update the root.
    *
    * @returns {ReactWrapper}
    */
   update() {
     if (this[ROOT] !== this) {
-      throw new Error('ReactWrapper::update() can only be called on the root');
+      return this[ROOT].update();
     }
     privateSetNodes(this, this[RENDERER].getNode());
     return this;
@@ -319,9 +319,6 @@ class ReactWrapper {
    * @returns {ReactWrapper}
    */
   setState(state, callback = undefined) {
-    if (this[ROOT] !== this) {
-      throw new Error('ReactWrapper::setState() can only be called on the root');
-    }
     if (this.instance() === null || this[RENDERER].getNode().nodeType !== 'class') {
       throw new Error('ReactWrapper::setState() can only be called on class components');
     }
@@ -681,9 +678,6 @@ class ReactWrapper {
    * @returns {*}
    */
   state(name) {
-    if (this[ROOT] !== this) {
-      throw new Error('ReactWrapper::state() can only be called on the root');
-    }
     if (this.instance() === null || this[RENDERER].getNode().nodeType !== 'class') {
       throw new Error('ReactWrapper::state() can only be called on class components');
     }
